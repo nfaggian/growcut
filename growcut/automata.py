58fa0a0172905cdce82ce794e0e458e5f0b88463
nfaggian <nathan.faggian@gmail.com>
2012-11-26 22:14:52 +1100

    Updates to the automata module.

--- growcut/automata.py
+++ growcut/automata.py
@@ -2,26 +2,31 @@
 
 import numpy as np
 
-from scipy import ndimage
-from scipy import misc
-
-
 NEIGHBOURS = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
 
 def iterNeighbours((r, c), neighbours=NEIGHBOURS):
     """ Yield the point neighborhood """
-
     for (dx, dy) in neighbours:
-        yield (r + dx, c + dy)
+        yield r + dx, c + dy
 
 
-# Numpy approach.
-def vonNeumannSampler(shape):
-    sr = []
-    sc = []
-    for (r, c) in np.ndindex(shape):
-        sr.append([r-1, r+1, r, r])
-        sc.append([c, c, c+1, c-1])
-    return np.array(sr), np.array(sc)
+def iterGrid(grid):
+    """ Iterate through a grid """
+    for point in np.ndindex(grid.shape):
+        values = [grid[x] for x in iterNeighbours(point)]
+        print '{}: {}'.format(point, values)
+
+
+# from scipy import ndimage
+# from scipy import misc
+
+# # Numpy approach.
+# def vonNeumannSampler(shape):
+#     sr = []
+#     sc = []
+#     for (r, c) in np.ndindex(shape):
+#         sr.append([r-1, r+1, r, r])
+#         sc.append([c, c, c+1, c-1])
+#     return np.array(sr), np.array(sc)
 
--- growcut/tests/test_automata.py
+++ growcut/tests/test_automata.py
@@ -2,6 +2,26 @@
 
 from growcut import automata
 
-def test_neighbours():
-    pass
+import numpy as np
 
+
+def test_neighbours():
+    """ Assert the pattern of the neighbourhood is correct """
+
+    point = (10, 10)
+    pointIterator = automata.iterNeighbours(point)
+
+    assert np.allclose(pointIterator.next(), (9, 10))
+    assert np.allclose(pointIterator.next(), (11, 10))
+    assert np.allclose(pointIterator.next(), (10, 11))
+    assert np.allclose(pointIterator.next(), (10, 9))
+
+
+def test_iterGrid():
+    """ Assert the correct pattern of samples """
+
+    rows, cols = np.mgrid[0:10, 0:10]
+
+    automata.iterGrid(rows)
+
+    assert False
