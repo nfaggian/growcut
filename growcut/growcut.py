ff5b490e5e00478020b03bdb26bf99957505709a
nfaggian <nathan.faggian@gmail.com>
2012-11-29 19:35:34 +1100

    Updates to a numpy version of growcut.

--- growcut/growcut.py
+++ growcut/growcut.py
@@ -2,18 +2,50 @@
 
 import numpy as np
 
+np.set_printoptions(threshold=100000)
+
 import automata
 
 
 def g(x, maxC):
     """ Damping function """
-    return 1. - (float(x) / maxC)
+    return 1. - ((x * 1.) / maxC)
 
 
 def norm(x):
     """ L2 norm """
     #return np.sqrt(np.sum(np.power(x, 2)))
     return np.linalg.norm(x)
+
+
+def numpyAutomate(lum, strength, label):
+    """ Numpy based grow-cut """
+
+    nextLabel = label.copy().flatten()
+    nextStrength = strength.copy().flatten()
+    coordinates = automata.formSamples(lum.shape, neighbours=automata.CONNECT_4)
+
+    CP = lum.flatten()
+    THETAP = strength.flatten()
+
+    CQ = automata.sample(lum, coordinates)
+    THETAQ = automata.sample(strength, coordinates)
+    LQ = automata.sample(label, coordinates)
+
+    CPminusCQ = np.vstack([CP] * CQ.shape[1]).T - CQ
+
+    attackStrength = g(CPminusCQ, lum.max()) * THETAQ
+    defendStrength = np.vstack([THETAP] * CQ.shape[1]).T
+
+    c = np.argmax(attackStrength > defendStrength, axis=1)
+    r = np.arange(0, attackStrength.shape[0])
+    mask = np.alltrue(~(attackStrength > defendStrength), axis=1)
+
+    # Fill the new strengths
+    nextLabel[mask] = LQ[r[mask], c[mask]]
+    nextStrength[mask] = attackStrength[r[mask], c[mask]]
+
+    return nextStrength.reshape(strength.shape), nextLabel.reshape(label.shape)
 
 
 def automate(lum, strength, label):
--- growcut/tests/test_growcut.py
+++ growcut/tests/test_growcut.py
@@ -19,16 +19,59 @@ def test_norm():
     pass
 
 
-def test_algorithm():
+# def test_automate():
 
-    shape = (20, 20)
+#     shape = (20, 20)
+#     cx, cy = shape[1] / 2.0, shape[0] / 2.0
+#     r = 5
+
+#     y, x = np.ogrid[0:shape[0], 0:shape[1]]
+#     mask = (np.power((y - cy), 2) + np.power((x - cx), 2)) < np.power(r, 2)
+
+#     field = np.zeros(mask.shape)
+#     field[mask] = 1.0
+#     label = np.zeros_like(field)
+#     theta = np.zeros_like(field)
+#     label[:] = 0
+
+#     label[0:(r / 4), 0:(r / 4)] = -1
+#     theta[0:(r / 4), 0:(r / 4)] = 1.0
+
+#     label[cy - (r / 4):cy + (r / 4), cx - (r / 4):cx + (r / 4)] = 1
+#     theta[cy - (r / 4):cy + (r / 4), cx - (r / 4):cx + (r / 4)] = 1.0
+
+#     # Plot the image and the label map.
+#     # import matplotlib.pyplot as plt
+#     # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+#     # ax1.imshow(field, interpolation='nearest', cmap='gray')
+#     # ax1.axis('off')
+#     # ax2.imshow(label, interpolation='nearest', cmap='jet')
+#     # ax2.axis('off')
+
+#     # Automate to update the labels
+#     for itteration in range(100):
+#         theta, label = growcut.automate(field, theta, label)
+
+#     # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+#     # ax1.imshow(field, interpolation='nearest', cmap='gray')
+#     # ax1.axis('off')
+#     # ax2.imshow(label, interpolation='nearest', cmap='jet')
+#     # ax2.axis('off')
+#     # plt.show()
+
+#     # Assert that the label map looks like the mask
+#     assert np.allclose(label[mask], 1.0) & np.allclose(label[~mask], -1), \
+#         "Segmentation did not converge after {} iterations".format(itteration)
+
+
+def test_numpy_automate():
+
+    shape = (10, 10)
     cx, cy = shape[1] / 2.0, shape[0] / 2.0
     r = 5
 
-    # Define a circular region of interest.
     y, x = np.ogrid[0:shape[0], 0:shape[1]]
-
-    mask = (np.power((y - cy), 2) + np.power((x - cx), 2)) < np.power(r, 2) + 2.0
+    mask = (np.power((y - cy), 2) + np.power((x - cx), 2)) < np.power(r, 2)
 
     field = np.zeros(mask.shape)
     field[mask] = 1.0
@@ -43,23 +86,23 @@ def test_algorithm():
     theta[cy - (r / 4):cy + (r / 4), cx - (r / 4):cx + (r / 4)] = 1.0
 
     # Plot the image and the label map.
-    # import matplotlib.pyplot as plt
-    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    # ax1.imshow(field, interpolation='nearest', cmap='gray')
-    # ax1.axis('off')
-    # ax2.imshow(label, interpolation='nearest', cmap='jet')
-    # ax2.axis('off')
+    import matplotlib.pyplot as plt
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    ax1.imshow(field, interpolation='nearest', cmap='gray')
+    ax1.axis('off')
+    ax2.imshow(label, interpolation='nearest', cmap='jet')
+    ax2.axis('off')
 
     # Automate to update the labels
-    for itteration in range(100):
-        theta, label = growcut.automate(field, theta, label)
+    for itteration in range(20):
+        theta, label = growcut.numpyAutomate(field, theta, label)
 
-    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    # ax1.imshow(field, interpolation='nearest', cmap='gray')
-    # ax1.axis('off')
-    # ax2.imshow(label, interpolation='nearest', cmap='jet')
-    # ax2.axis('off')
-    # plt.show()
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    ax1.imshow(field, interpolation='nearest', cmap='gray')
+    ax1.axis('off')
+    ax2.imshow(label, interpolation='nearest', cmap='jet')
+    ax2.axis('off')
+    plt.show()
 
     # Assert that the label map looks like the mask
     assert np.allclose(label[mask], 1.0) & np.allclose(label[~mask], -1), \
